2e48685f2315151ea0f3c50251ca6b0c2fa5153e
Dan Sehl <dan.sehl@gmail.com>
2020-06-10 09:14:21 -0600

    Write test for #stock method for vendor

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -23,4 +23,13 @@ class VendorTest < Minitest::Test
     assert_equal 0, vendor.check_stock(item1)
   end
 
+  def test_it_can_stock_items
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    vendor.stock(item1, 30)
+    expected = {item1 => 30}
+    assert_equal expected, vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+  end
+
 end
