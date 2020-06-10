fdd6cd8a55259658d71763ffad64247e57ec2d13
Dan Sehl <dan.sehl@gmail.com>
2020-06-10 09:11:26 -0600

    Write test to check for stock for vendor

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require './lib/item'
 
 class VendorTest < Minitest::Test
 
@@ -14,6 +15,12 @@ class VendorTest < Minitest::Test
     assert_equal "Rocky Mountain Fresh", vendor.name
     expected = {}
     assert_equal expected, vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    assert_equal 0, vendor.check_stock(item1)
   end
 
 end
