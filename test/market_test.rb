00416f541d0ba807e1af6b8d99de862f971ff120
Dan Sehl <dan.sehl@gmail.com>
2020-06-10 09:25:02 -0600

    Write test attributes test for market

--- test/market_test.rb
+++ test/market_test.rb
@@ -11,4 +11,10 @@ class MarketTest < Minitest::Test
     assert_instance_of Market, market
   end
 
+  def test_it_has_a_name_and_no_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+
 end
